eeac30f3d14bcb65f2d25279c76c0d41bacad1a0
Andrew Sadowski <andrewsadowski@MBP.local>
2017-06-04 18:43:30 -0700

    First commit

---
+++ fsStream.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+
+var readableStream = fs.createReadStream('CS_CZ_iOS.txt');
+var writableStream = fs.createWriteStream('output.txt');
+
+var search = '/****************** Version - 2.0.0 **********************/';
+
+var body = fs.readFileSync('CS_CZ_iOS.txt').toString();
+
+var afterSearch = body.substr(body.indexOf(search));
+
+fs.writeFile('output.txt', afterSearch, (err) => {
+    if(err) {
+        return console.log(err);
+    }
+    console.log('The file was saved');
+});
+console.log(afterSearch);
+
+if (body.indexOf(search) > 0) {
+    console.log('FOUND');
+} else {
+    console.log('not found');
+}
+
